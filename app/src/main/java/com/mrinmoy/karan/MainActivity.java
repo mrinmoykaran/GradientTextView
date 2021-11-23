45c1a8a69e7032d26e6417d23e5b4eb2ac5f7d8b
Mrinmoy Karan <srvice.mrinmoykaran@gmail.com>
2021-11-23 09:37:02 +0530

    Initial commit

---
+++ app/src/main/java/com/mrinmoy/karan/MainActivity.java
@@ -0,0 +1,25 @@
+package com.mrinmoy.karan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+   // GradientTextView gradientTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+     //   gradientTextView=findViewById(R.id.txt5);
+        //gradientTextView.setStartColor(Color.RED);
+       // gradientTextView.setCenterColor(Color.GREEN);
+      //  gradientTextView.setEndColor(Color.WHITE);
+       // gradientTextView.setAngle(45);
+
+
+    }
+}
